7bec18dcc63e2c04cdf5a27063f56452a438efcd
troyano315 <luciano315@hotmail.com>
2022-10-13 16:04:13 -0400

    Created Vue app, Mounted App, Displayed the data

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            description: 'Awarm fuzzy pair of socks'
+        }
+    }
+})
