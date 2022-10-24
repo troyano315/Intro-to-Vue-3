0d3a002d53446152b7056071c73b4d1932ae49a1
hjohns204 <hector_johnsonluciano@cable.comcast.com>
2022-10-24 10:36:36 -0400

    added new button to ProductDisplay which removes product from the cart added product id and emitted event from component

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,15 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button 
+        class="button" 
+        :class="{ disabledButton: !inStock }" 
+        :disabled="!inStock" 
+        v-on:click="removeFromCart">
+        Remove Item
+      </button>
+
       </div>
     </div>
   </div>`,
@@ -56,11 +65,14 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-          this.cart += 1
+          this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
-      }
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
+    }
   },
   computed: {
       title() {
